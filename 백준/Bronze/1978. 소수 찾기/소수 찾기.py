97f2a364a56474a27487f239b45d58f39c11ce3e
TaeGyun Kim <kimtg0003@gmail.com>
2024-12-24 16:14:43 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,18 @@
+count = int(input())
+
+numbers = map(int, input().split())
+
+result = 0
+
+
+for i in numbers:
+    error = 0
+    if i > 1:
+        for j in range(2, i):
+            if i%j == 0:
+                error += 1
+                
+                
+        if error == 0:
+            result += 1
+print(result)
